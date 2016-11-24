e578062f352e0d043e83b2c28d1f365467d5ea10
mrhanick <761663701@qq.com>
2016-11-24 16:51:47 +0800

    322

--- app/src/main/java/com/yimi/gittest01/MainActivity.java
+++ app/src/main/java/com/yimi/gittest01/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //1111
+        //11112323
     }
 }
